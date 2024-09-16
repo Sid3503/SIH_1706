4125a1cb3da55e23bd40101bc6def139bf0997a1
Siddharth Mishra <120008126+Sid3503@users.noreply.github.com>
2024-09-16 23:02:30 +0530

    Create db.py
    Dummy Data

---
+++ db.py
@@ -0,0 +1,7 @@
+employees = {
+    'emp1': {'employee_id': '001', 'password': 'pass123', 'email': 'emp1@example.com'},
+    'emp2': {'employee_id': '002', 'password': 'pass456', 'email': 'emp2@example.com'},
+    'emp3': {'employee_id': '003', 'password': 'pass789', 'email': 'emp3@example.com'},
+    'emp4': {'employee_id': '004', 'password': 'pass012', 'email': 'emp4@example.com'},
+    'emp5': {'employee_id': '005', 'password': 'pass345', 'email': 'emp5@example.com'}
+}
